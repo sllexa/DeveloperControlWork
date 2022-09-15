598a5addbf2325ee471d1af937c4d4dbbc21221f
Aleksei Slivko <sllexa@mail.ru>
2022-09-15 16:24:10 +0300

    Task solver added

---
+++ Test_work/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] Where(string[] array)
+{
+    int size = 0;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= 3) size++;
+    }
+    string[] data = new string[size];
+    int index = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            data[index] = array[i];
+            index++;
+        }
+    }
+    return data;
+}
+
+string[] arrStr = { "hello", "2", "world", "res", "75", "date" };
+string[] arrData = Where(arrStr);
+Console.WriteLine(string.Join(", ", arrData));
